7322de657dc818c9567327dbe7a16678a5adc2e0
Andrew Piatetsky <apiatetsky30@stuy.edu>
2025-06-27 18:32:18 -0400

    changed to more stable version for template and js

--- live/urls.py
+++ live/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path("api/message/", api.trigger_write, name='trigger_write'),
     path("api/message/<str:task_id>/", api.get_result_message),
     path("api/progress/<str:task_id>/", api.task_progress),
-    path("api/download/<str:task_id>/", api.download_result, name="download_result"),
+    # path("api/download/<str:task_id>/", api.download_result, name="download_result"),
 ]
