da7521a997e3137e3e66dda190de927c29343580
Andrew Piatetsky <apiatetsky30@stuy.edu>
2025-09-01 14:40:29 -0400

    added fixes and static file serving

--- docker/django/django.entry.sh
+++ docker/django/django.entry.sh
@@ -5,4 +5,6 @@ trap 'echo "[ERROR] Command failed at line $LINENO: $BASH_COMMAND" >&2' ERR
 
 # python manage.py runserver 0.0.0.0:8000
 
-uvicorn canopy.asgi:application --host 0.0.0.0 --port 8000 --reload
+python manage.py collectstatic --noinput
+
+exec uvicorn canopy.asgi:application --host 0.0.0.0 --port 8000 --reload
